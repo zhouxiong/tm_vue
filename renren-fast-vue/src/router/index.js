7bce9849022c099ccebb8e23b67b4533530c560c
zhouxiong <706340089@qq.com>
2019-12-25 15:59:32 +0800

    路由提交

--- renren-fast-vue/src/router/index.js
+++ renren-fast-vue/src/router/index.js
@@ -26,14 +26,14 @@ const mainRoutes = {
   path: '/',
   component: _import('main'),
   name: 'main',
-  redirect: { name: 'home' },
+  redirect: { name: 'demo-info' },
   meta: { title: '主入口整体布局' },
   children: [
     // 通过meta对象设置路由展示方式
     // 1. isTab: 是否通过tab展示内容, true: 是, false: 否
     // 2. iframeUrl: 是否通过iframe嵌套展示内容, '以http[s]://开头': 是, '': 否
     // 提示: 如需要通过iframe嵌套展示内容, 但不通过tab打开, 请自行创建组件使用iframe处理!
-    { path: '/home', component: _import('common/home'), name: 'home', meta: { title: '首页' } },
+    { path: '/cailiao', component: _import('modules/cailiao/wisdplatcailiao'), name: 'home', meta: { title: '首页' } },
     { path: '/theme', component: _import('common/theme'), name: 'theme', meta: { title: '主题' } },
     { path: '/demo-info', component: _import('demo/info/wisdplatcailiao'), name: 'demo-info', meta: { title: '材料信息' } },
     { path: '/demo-echarts', component: _import('demo/echarts'), name: 'demo-echarts', meta: { title: 'demo-echarts', isTab: true } },
