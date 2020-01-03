9aa4e0500de3b15878cd5020990faff01433cb57
wlm <814874811@qq.com>
2020-01-03 17:36:34 +0800

    yanzhengma

--- renren-fast-vue/src/views/common/login.vue
+++ renren-fast-vue/src/views/common/login.vue
@@ -15,7 +15,7 @@
             <el-form-item prop="password">
               <el-input v-model="dataForm.password" type="password" placeholder="密码"></el-input>
             </el-form-item>
-            <el-form-item prop="captcha">
+            <!-- <el-form-item prop="captcha">
               <el-row :gutter="20">
                 <el-col :span="14">
                   <el-input v-model="dataForm.captcha" placeholder="验证码">
@@ -25,7 +25,7 @@
                   <img :src="captchaPath" @click="getCaptcha()" alt="">
                 </el-col>
               </el-row>
-            </el-form-item>
+            </el-form-item> -->
             <el-form-item>
               <el-button class="login-btn-submit" type="primary" @click="dataFormSubmit()">登录</el-button>
             </el-form-item>
@@ -54,15 +54,15 @@
           password: [
             { required: true, message: '密码不能为空', trigger: 'blur' }
           ],
-          captcha: [
-            { required: true, message: '验证码不能为空', trigger: 'blur' }
-          ]
+          // captcha: [
+          //   { required: true, message: '验证码不能为空', trigger: 'blur' }
+          // ]
         },
         captchaPath: ''
       }
     },
     created () {
-      this.getCaptcha()
+     // this.getCaptcha()
     },
     methods: {
       // 提交表单
@@ -76,14 +76,14 @@
                 'username': this.dataForm.userName,
                 'password': this.dataForm.password,
                 'uuid': this.dataForm.uuid,
-                'captcha': this.dataForm.captcha
+                // 'captcha': this.dataForm.captcha
               })
             }).then(({data}) => {
               if (data && data.code === 0) {
                 this.$cookie.set('token', data.token)
                 this.$router.replace({ name: 'home' })
               } else {
-                this.getCaptcha()
+                //this.getCaptcha()
                 this.$message.error(data.msg)
               }
             })
@@ -91,10 +91,10 @@
         })
       },
       // 获取验证码
-      getCaptcha () {
-        this.dataForm.uuid = getUUID()
-        this.captchaPath = this.$http.adornUrl(`/captcha.jpg?uuid=${this.dataForm.uuid}`)
-      }
+      // getCaptcha () {
+      //   this.dataForm.uuid = getUUID()
+      //   this.captchaPath = this.$http.adornUrl(`/captcha.jpg?uuid=${this.dataForm.uuid}`)
+      // }
     }
   }
 </script>
